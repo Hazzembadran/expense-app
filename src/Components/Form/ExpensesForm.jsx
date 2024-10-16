e4d2c3772bb26b6aff38a082aed4544695d918e9
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 23:23:07 +0300

    Use the useRef hock to get input value

--- src/Components/Form/ExpensesForm.jsx
+++ src/Components/Form/ExpensesForm.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import ExpensesFormInput from "./ExpensesFormInput";
 
 const ExpensesForm = () => {
+  let titleRef = useRef();
+  let dateRef = useRef();
+  let valueRef = useRef();
+  let descriptionRef = useRef();
+
+  let onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(titleRef.current.value);
+  };
+
   return (
-    <form className="row">
-      <ExpensesFormInput title="Title" type="text" classInput="addTitle" />
-      <ExpensesFormInput title="Date" type="date" classInput="addDate" />
-      <ExpensesFormInput title="Value" type="text" classInput="addValue" />
+    <form className="row" onSubmit={onSubmitHandler}>
+      <ExpensesFormInput
+        title="Title"
+        type="text"
+        classInput="addTitle"
+        ref={titleRef}
+      />
+      <ExpensesFormInput
+        title="Date"
+        type="date"
+        classInput="addDate"
+        ref={dateRef}
+      />
+      <ExpensesFormInput
+        title="Value"
+        type="text"
+        classInput="addValue"
+        ref={valueRef}
+      />
       <ExpensesFormInput
         title="Description"
         type="text"
         classInput="addDescription"
+        ref={descriptionRef}
       />
 
       <div className="mb-3 col-md-12 text-right">
