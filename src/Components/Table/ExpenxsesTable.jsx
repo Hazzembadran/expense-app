eedb858c1508172bb83db3b6ac406836835bd176
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 21:57:59 +0300

    Create ExpenxsesTable

---
+++ src/Components/Table/ExpenxsesTable.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ExpenxsesTableRow from "./ExpenxsesTableRow";
+
+const ExpenxsesTable = () => {
+  return (
+    <table class="table ">
+      <thead>
+        <tr>
+          <th> Title</th>
+          <th> Date</th>
+          <th>value</th>
+          <th>Description</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <ExpenxsesTableRow />
+      </tbody>
+    </table>
+  );
+};
+
+export default ExpenxsesTable;
