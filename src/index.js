662a8bb245fb06c07a635b887ae538124e536127
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 14:56:47 +0300

    Add the main layout to index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainLayout from './pages/MainLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <MainLayout />
 );
 
