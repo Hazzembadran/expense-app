23ef459ef162bf0578251d50e7f8484e8731a56f
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 21:56:56 +0300

    Create ExpensesImage

---
+++ src/Components/Info/ExpensesImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpensImage from "../../Resources/img/m1.png"
+
+const ExpensesImage = () => {
+  return (
+    <div class="col-sm-6">
+      <img src={ExpensImage} class="img-fluid" alt="ExpensImage" />
+    </div>
+  );
+};
+
+export default ExpensesImage;
