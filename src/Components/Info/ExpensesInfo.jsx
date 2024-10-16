a9c23124e68348f04b82a0eb18bddf78e6ae984d
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 21:57:10 +0300

    Create ExpensesInfo

---
+++ src/Components/Info/ExpensesInfo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ExpensesInfo = () => {
+  return (
+    <div class="row tit">
+      <h4 class="">Wellcom to Hazem Expense Manager </h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam
+      </p>
+    </div>
+  );
+};
+
+export default ExpensesInfo;
