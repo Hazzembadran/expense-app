7889cde87e93729a3195952c84d3265ece12d37e
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 23:24:10 +0300

    use forwardRef in component

--- src/Components/Form/ExpensesFormInput.jsx
+++ src/Components/Form/ExpensesFormInput.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-const ExpensesFormInput = ({ title, type, classInput }) => {
+const ExpensesFormInput = React.forwardRef((props, ref) => {
   return (
     <div className="mb-3 col-md-6">
-      <label className="form-label">{title}</label>
+      <label className="form-label">{props.title}</label>
       <input
-        type={type}
-        className={`form-control ${classInput}`}
+        type={props.type}
+        className={`form-control ${props.classInput}`}
         aria-describedby=""
+        ref={ref}
       />
     </div>
   );
-};
+});
+
+
 
 export default ExpensesFormInput;
