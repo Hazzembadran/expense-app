847658b66ab72855894048a0193475c74b9f2b95
devhazemaq <hazemahmedqindeel@gmail.com>
2024-10-16 23:31:49 +0300

    ExpensesModel it is an object to collect the form data

---
+++ src/Model/ExpensesModel.jsx
@@ -0,0 +1,10 @@
+class ExpensesModel{
+  constructor(title, date, value, description) {
+    this.title = title;
+    this.date = date;
+    this.value = value;
+    this.description = description;
+  }
+}
+
+export default ExpensesModel
